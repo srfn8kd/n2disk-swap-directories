8a148b8631b5a5cbf889b1a4772126a4c7e073b9
Greg Grasmehr <srfn8kd@users.noreply.github.com>
2021-02-18 16:27:37 -0800

    Update n2disk-swap.sh
    Duh should have originally negated the match instead of using an else... ;)

--- n2disk-swap.sh
+++ n2disk-swap.sh
@@ -12,10 +12,8 @@ if [ $? -eq 0 ]
 then
 Z1=`/bin/cat /tmp/last-data2-file`
 Z2=`/bin/find /data2/ -type f -name $Z1`
-if [[ $Z2 =~ [a-zA-Z] ]]
+if [[ ! $Z2 =~ [a-zA-Z] ]]
 then
-sleep 2
-else
 echo "Updating n2disk data dir to data3"
 /bin/ls -lR /data3 | /bin/tail -n 1 | /bin/awk '{print $9}' > /tmp/last-data3-file
 /usr/bin/sed -i 's/data2/data3/' $ZFILE
@@ -25,10 +23,8 @@ fi
 else
 Z1=`/bin/cat /tmp/last-data3-file`
 Z2=`/bin/find /data3/ -type f -name $Z1`
-if [[ $Z2 =~ [a-zA-Z] ]]
+if [[ ! $Z2 =~ [a-zA-Z] ]]
 then
-sleep 2
-else
 echo "Updating n2disk data dir to data2"
 /bin/ls -lR /data2 | /bin/tail -n 1 | /bin/awk '{print $9}' > /tmp/last-data2-file
 /usr/bin/sed -i 's/data3/data2/' $ZFILE
